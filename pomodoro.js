11e2c4ee1e140441ec461f82de242079df20b44c
RoisinOk <roisin_ok@hotmail.com>
2015-07-28 13:56:21 +0100

    Made button to start timer. Formatted time to show double digits. Seperate buttons to pause and resume timer, but only work in the right order.

--- pomodoro.js
+++ pomodoro.js
@@ -6,31 +6,45 @@
 //milliseconds. It takes two parameters, the first is a function, the second is a number of milliseconds.
 //1 second = 1000 milliseconds
 
-var timerVar = window.setInterval(function(){ myTimer() }, 100);    //Needs global variable so clearInterval can refernce it
-
-var start = new Date().getTime();       //Return the number of milliseconds since midnight 1970/01/01
+var timerVar;    //Needs global variable so clearInterval can reference it
+var start;                              //Declare variable for start time
 var secsElapsed;                        //Declare variable for elapsed secs
 var minsElapsed;                        //Declare variable for elapsed mins
 
 var secsLeft = 120;                      //sets seconds to 60. Counts down from this number.
 var minsLeft = secsLeft/60;             //Amount of minutes in secsLeft
 
+function startTimer(){
+    timerVar = window.setInterval(function(){ myTimer() }, 100);
+    start = new Date().getTime();       //Return the number of milliseconds since midnight 1970/01/01
+}
+
 function myTimer(){
     var time = new Date().getTime() - start;                        //Gets amount of milliseconds that have passed since start
     secsElapsed = Math.floor(time / 1000);                          //Rounds number to a second
     minsElapsed = (Math.floor(time / 1000)/60);                     //Converts to minutes to show amount of minutes that have passed
-    document.getElementById("secsDiv").innerHTML = (secsLeft -(secsElapsed))%60;            //Seconds left, minus seconds elapsed. Displays remainder after divided by 60.
-    document.getElementById("minsDiv").innerHTML = Math.floor( minsLeft -(minsElapsed));    //Displays starting amount of mins, minus mins elapsed.
+    var secsDisplay = (secsLeft -(secsElapsed))%60;                 //Seconds left, minus seconds elapsed. Displays remainder after divided by 60.
+    var minsDisplay = Math.floor( minsLeft -(minsElapsed));         //Displays starting amount of mins, minus mins elapsed.
+    if (secsDisplay<10){                                                    //Checks if secsDisplay is single digit
+        document.getElementById("secsDiv").innerHTML = "0"+secsDisplay;     //Adds zero before if single
+    }else{
+        document.getElementById("secsDiv").innerHTML = secsDisplay;         //Displays normally if not.
+    }
+    if (minsDisplay<10){                                                    //Checks if minsDisplay is single digit
+        document.getElementById("minsDiv").innerHTML = "0"+minsDisplay;     //Adds zero before it if single
+    }else{
+        document.getElementById("minsDiv").innerHTML = minsDisplay;         //Displays normally if not.
+    }
 }//end of function myTimer
 
 function pauseTimer(){
+    secsLeft = secsLeft -(secsElapsed);
+    minsLeft = minsLeft -(minsElapsed);
     clearInterval(timerVar);            //stops the timerVar setInterval method from running
 }
 
 //Function resumeTimer is the same as start timer, but secs and start variables are updated.
 function resumeTimer(){
-    secsLeft = document.getElementById("time").innerHTML;               //updates global variable 'secs' to time countdown stops (string)
-    secsLeft = parseInt(secsLeft);                                          //parses this string to an integer
     start = new Date().getTime();                                   //updates start variable to new current time millisecs
     timerVar = window.setInterval(function(){ myTimer() }, 100);    //runs timer var function again with new secs (from pause function)
 };
